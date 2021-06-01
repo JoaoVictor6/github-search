eec470b473296f7fe4ca839cc6b2a8ba35420810
João Victor <santosjoao.dev@gmail.com>
2021-06-01 11:21:14 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ProfilePage from './components/ProfilePage';
 import SearchPage from './components/SearchPage';
 import { SearchContext } from './context/SearchContext';
 
 function App():JSX.Element{
   return (
   <SearchContext> 
-    <SearchPage />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={ SearchPage } />
+        <Route path='/user/:username' component={ ProfilePage } />
+      </Switch>
+    </BrowserRouter>
   </SearchContext> 
   );
 }
--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import useSearchContext from '../../context/SearchContext';
+// import useSearchContext from '../../context/SearchContext';
 
 import { Container } from './styles';
 
 const ProfilePage: React.FC = () => {
-  const { searchValue } = useSearchContext();
+  // const { searchValue } = useSearchContext();
   const [ apiResponse, setApiResponse ] = useState([])
 
   useEffect( () => {
     const api = async () => {
-      const response = await fetch(`https://api.github.com/users/${searchValue}/repos`)
-      const data = await response.json()
+      // const response = await fetch(`https://api.github.com/users/${searchValue}/repos`)
+      // const data = await response.json()
 
-      setApiResponse(data)
+      setApiResponse([])
     }
 
     api();
