88a0e8f3cc0f9237a0088bf2e0a2e121ed22b40e
João Victor <santosjoao.dev@gmail.com>
2021-06-05 11:18:05 -0300

    fix: add semicolon in the end  of the line

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
   body{
     margin: 0;
     padding: 0;
-    background: ${({ theme }: { theme: PropsTheme }) => theme.background}
+    background: ${({ theme }: { theme: PropsTheme }) => theme.background};
   }
 
   * {
