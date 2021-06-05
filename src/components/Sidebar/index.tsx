6bacfb2314d381cef6cc44e47c7b8c9122a39caa
João Victor <santosjoao.dev@gmail.com>
2021-06-05 11:26:26 -0300

    feat: add sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,18 @@
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 
-import { Container } from './styles';
+import { Container,
+  Header,
+  Img,
+  InfoTitles,
+  UserNumbers,
+  Content,
+  Bio,
+  Name,
+  FooterInformation,
+  Icons } from './styles';
 
 export interface UserInformation{
   login: string;
@@ -14,13 +26,41 @@ export interface UserInformation{
   blog: string;
   twitter_username: string;
   message?: string;
+  html_url?: string;
 }
 
 
 function Sidebar({ user }: { user:UserInformation }):JSX.Element {
   return (
     <Container>
-      <img src={user.avatar_url} alt="user logo" />
+      <Header>
+      <Img src={user.avatar_url} alt="user logo" />
+      <UserNumbers>
+        <InfoTitles>{user.login}</InfoTitles>
+        <InfoTitles>Followers</InfoTitles>
+        {user.followers}
+        <InfoTitles>Following</InfoTitles>
+        {user.following}
+      </UserNumbers>
+      </Header>
+      <Content>
+        <Bio>{user.bio}</Bio>
+        <Name>{user.name}</Name>
+
+        <FooterInformation>
+          Company: {user.company}<br />
+          Location: {user.location}
+
+          <Icons>
+            <a href={user.blog || user.html_url} target='_blank' rel="noreferrer">
+              <FontAwesomeIcon icon={faSignOutAlt}/>
+            </a>
+            <a href={`https://twitter.com/${user.twitter_username || ''}`} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={faTwitter}/>
+            </a>
+          </Icons>
+        </FooterInformation>
+      </Content>
     </Container>
   );
 }
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,3 +1,94 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  box-shadow: 1px 0px 1px -1px rgba(0,0,0,0.55);
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 400px;
+  color: #383838;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  margin: 20px 16px;
+  width: calc(100% - 32px);
+  font-size: 18px;
+`
+
+export const Img = styled.img`
+  height: 200px;
+  width: 200px;
+  border-radius: 10px;
+`
+
+export const UserNumbers = styled.div`
+  width: calc(100%);
+  text-align: center;
+`;
+
+export const InfoTitles = styled.h1`
+  &&:first-child{
+    font-family: 'Nunito', sans-serif;
+    font-weight: 700;
+    font-size: 26px;
+    margin-top: 0;
+  }
+  margin: 0px;
+  margin-top: 15px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 18px;
+  font-weight: 600;
+`
+
+export const Content = styled.div`
+ margin: 0 16px;
+ width: calc(100% - 32px);
+`
+
+export const Bio = styled.p`
+  text-align: center;
+  font-style: italic;
+  color: #4A4A4A;
+  font-size: 20px;
+  margin: 0;
+`
+
+export const Name = styled.h1`
+  color: #303030;
+  font-size: 20px;
+  font-weight: normal;
+  margin-top: 0px;
+  text-align: center;
+  margin: 10px;
+`
+
+export const FooterInformation = styled.div`
+  margin-top: 7em;
+  font-size: 19px;
+  font-weight: 500;
+`;
+
+export const Icons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px auto;
+  width: 30%;
+  && > a{
+    text-decoration: none;
+    color: #383838;
+  }
+
+  && > a:hover{
+    color: #000;
+  }
+
+  && > a:nth-child(2):hover {
+    color: #2E9BFF;
+  }
+
+  && > a > svg{
+    font-size: 36px;
+  }
+` 
